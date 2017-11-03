b94aa822c3a6ba270fd7d6c4eb95d0b9e1de5384
fredesse <eleegia@gmail.com>
2017-11-02 17:21:05 -0700

    Create search view structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: '',
+      longitude: '',
+      city: '',
+    }
+  }
+
+  getLocation = () => {
+    let showPosition = (position) => {
+      this.setState({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      });
+      console.log("STATE", this.state);
+    }
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+      console.log("DONE!");
+    } else {
+      alert("Current location is not supported by this browser");
+    }
+  }
+
+  handleSearch = (event) => {
+    this.setState({
+      city: event.target.value
+    });
+    console.log("CITY", this.state.city);
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let data = {
+
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="">
+        <div>
+          <form>
+            <input type="text" value={this.state.city} placeholder="City" onChange={this.handleSearch}/>
+            <input type="submit"/>
+          </form>
+        </div>
+        <div>
+          <p>or</p>
+        </div>
+        <div>
+          <p>use my <a onClick={() => { this.getLocation()}}>current position</a></p>
+        </div>
       </div>
     );
   }
